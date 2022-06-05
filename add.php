b646f731297d219eeff4826241d94ea8b3543984
Emiliano Badillo <emiliano.badillo7@gmail.com>
2022-06-04 19:59:04 -0500

    Add pomodoro

--- add.php
+++ add.php
@@ -53,7 +53,8 @@
                                 <h2>Lista <b>Actividades</b></h2>
                             </div>
                             <div class="col-sm-6">
-                                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar Actividad</span></a>						
+                                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar Actividad</span></a>
+                                <a href=pomodoro.php class="btn btn-warning" style= color:black;><i class="material-icons">&#xE037;</i><span>Iniciar pomodoro</span></a>					
                             </div>
                         </div>
                     </div>
@@ -90,6 +91,7 @@
                                 if ($row["owner"] == $_SESSION["username"]) {
                                     echo 
                                     "<tr>
+                                        <td style='display:none;''>" . $row["id"] . "</td>
                                         <td>" . $row["owner"] . "</td>
                                         <td>" . $row["status"] . "</td>
                                         <td>" . $row["description"] . "</td>
@@ -146,27 +148,27 @@
         <div id="editEmployeeModal" class="modal fade">
             <div class="modal-dialog">
                 <div class="modal-content">
-                    <form>
+                    <form action="includes/update.inc.php?id=$row['id']" method="POST">
                         <div class="modal-header">						
                             <h4 class="modal-title">Edit Employee</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                         </div>
                         <div class="modal-body">					
                             <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" class="form-control" required>
+                                <label>Usuario</label>
+                                <input type="text" value="<?php echo $_SESSION["username"]; ?>" name="owner" class="form-control" required readonly>
                             </div>
                             <div class="form-group">
-                                <label>Email</label>
-                                <input type="email" class="form-control" required>
+                                <label>Estatus</label>
+                                <input type="number" name="status" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label>Address</label>
-                                <textarea class="form-control" required></textarea>
+                                <label>Descripcion</label>
+                                <textarea class="form-control" name="description" required></textarea>
                             </div>
                             <div class="form-group">
-                                <label>Phone</label>
-                                <input type="text" class="form-control" required>
+                                <label>Fecha</label>
+                                <input type="date" name="date" class="form-control" required>
                             </div>					
                         </div>
                         <div class="modal-footer">
---
+++ pomodoro.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pomodoro</title>
+    <link rel="stylesheet" href="css/pomodoro.css" />
+  </head>
+  <body>
+    <h1>Pomodro ⏲ con 🛎</h1>
+
+    <figure class="clock">
+      <div class="mins">0</div>
+      <div>:</div>
+      <div class="secs">00</div>
+      <audio
+        src="http://soundbible.com/mp3/service-bell_daniel_simion.mp3"
+      ></audio>
+      <svg class="progress-ring" height="120" width="120">
+        <circle
+          class="progress-ring__circle"
+          stroke-width="8"
+          fill="transparent"
+          r="50"
+          cx="60"
+          cy="60"
+        />
+      </svg>
+    </figure>
+
+    <div class="btn-group">
+      <button class="start">comenzar</button>
+      <button class="reset">reiniciar</button>
+      <button class="pause">pausar</button>
+    </div>
+
+    <form action=".">
+      <label for="focusTime">Tiempo de Concentracion</label>
+      <input type="number" value="1" id="focusTime" />
+      <label for="breakTime">Tiempo de Descanso</label>
+      <input type="number" value="1" id="breakTime" />
+      <button type="submit">Guardar ajustes</button>
+    </form>
+
+    <script src="js/settings.js"></script>
+    <script src="js/timer.js"></script>
+    <script src="js/progress.js"></script>
+  </body>
+</html>
