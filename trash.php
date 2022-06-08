45874173b1727abbab97925b36b361536bf85938
Emiliano Badillo <emiliano.badillo7@gmail.com>
2022-06-08 18:43:10 -0500

    Fix typos and css

--- trash.php
+++ trash.php
@@ -50,7 +50,7 @@
                     <div class="table-title">
                         <div class="row">
                             <div class="col-sm-6">
-                                <h2>Lista <b>Actividades</b></h2>
+                                <h2>Lista Actividades <b>Finalizadas</b></h2>
                             </div>
                             <div class="col-sm-6">
                                 <!--<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar Actividad</span></a>
